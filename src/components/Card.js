d22449acd23767174730d76f69e7dde0647d054e
computerbox <computerbox124@gmail.com>
2023-12-24 18:47:51 +0400

    Modified Card Component, fetched API data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,38 @@
-function Card (props){
-    return (
-        <div className="card">
-            <img className="card-img-top" src={props.img} alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">{props.title}</h5>
-                    <p className="card-text">{props.text}</p>
+function getTime(time){
+  const dateObj = new Date(time)
+  const date = dateObj.getDate();
+  const month = dateObj.getMonth();
+  const hours = dateObj.getHours();
+  const minutes = dateObj.getMinutes();
+  const seconds = dateObj.getSeconds();
+  const months = ["January", "February", "March", "April", "May",
+      "June", "July", "August", "September", "October", "November", "December"]
+  return 'Created/Modified: ' + date + ' ' + months[month] + ' ' + hours + ':' + minutes + ':' + seconds;
+}
 
-                    <a href="#" className="btn btn-primary"></a>
-                </div>
+function Card ({props}){
+
+    return (
+        <div className="card h-100">
+            {
+                props.status === "Learned" ?
+                    <div className="card-header" style={{color: "white", background: "green"}}>{props.status}</div>:
+                props.status === 'Want to Learn' ?
+                    <div className="card-header" style={{color: "white", background: "red"}}>{props.status}</div>:
+                    <div className="card-header" style={{color: "white", background: "yellow"}}>{props.status}</div>
+
+            }
+
+            <div className="card-body">
+                    <p className="card-text">{props.content}</p>
+                    {props.img !== "none" ? <img className="card-img" src={props.img} alt="Card image cap" /> : <div />}
+            </div>
+
+            <div className="card-footer">
+                <p className="card-text">
+                    {getTime(props.date)}
+                </p>
+            </div>
         </div>
     );
 }
--- src/components/Learning.js
+++ src/components/Learning.js
@@ -1,9 +1,46 @@
 import Card from "./Card";
+import {useEffect, useState} from "react";
 
 function Learning (){
+    const [data, setData] = useState([]);
+    const urlAPI = process.env.REACT_APP_BACKEND_API;
+
+    useEffect(() => {
+        const dataFetch = async () => {
+            try {
+                const response = await fetch(urlAPI + '/cards');
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        dataFetch();
+    }, [urlAPI]);
+
+
+    // Splitting cards to the rows
+    const cardsRow = 4; // Count of cards in each row
+    const rows = [];
+    for (let i = 0; i < data.length; i += cardsRow) {
+        rows.push(data.slice(i, i + cardsRow));
+    }
+
     return (
-        <div>
-           <Card img="https://codeany.org/media/pictures/article-covers/azeioi2023.jpg" title="I" text="hi"/>
+        <div className="flexWrapper">
+            <div className="container">
+
+                {rows.map((row, indexRow) => (
+                    <div key={indexRow} className="row mb-4 gy-2">
+                        {row.map(card => (
+                            <div key={card.id} className="col">
+                             <Card props={{...card}} />
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
